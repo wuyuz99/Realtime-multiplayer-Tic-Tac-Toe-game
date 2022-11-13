3401cdd3388fba52d8e4ba3243744aa61a640bcd
isaac0A0 <isaac.wuyuz@gmail.com>
2022-11-13 15:32:12 -0800

    initialize java backend

---
+++ backend/real-time-tic-tac-toe/real-time-tic-tac-toe/src/main/java/edu/scu/coen275/realtimetictactoe/RealTimeTicTacToeApplication.java
@@ -0,0 +1,13 @@
+package edu.scu.coen275.realtimetictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeTicTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeTicTacToeApplication.class, args);
+	}
+
+}
---
+++ backend/real-time-tic-tac-toe/real-time-tic-tac-toe/src/test/java/edu/scu/coen275/realtimetictactoe/RealTimeTicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.scu.coen275.realtimetictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeTicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
