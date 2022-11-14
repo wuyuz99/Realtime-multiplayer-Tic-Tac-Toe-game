ba683a41bbd24b267aad2d914fb5946a40ce28e8
isaac0A0 <isaac.wuyuz@gmail.com>
2022-11-13 16:31:59 -0800

    add up until connectToGame

---
+++ backend/TicTacToe/src/test/java/edu/scu/coen275/TicTacToe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.scu.coen275.TicTacToe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
